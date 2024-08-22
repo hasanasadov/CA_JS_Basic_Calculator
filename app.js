8e008237169ae8e3732f9918ca8cecbd52f29a83
hasanasadov <hasanaliasadov@gmail.com>
2024-08-22 21:27:04 +0400

    prettier

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-const inputs = document.querySelectorAll('input');
-const result = document.querySelector('#result');
-const calculateBtn = document.querySelector('#calculate');
-const resetBtn = document.querySelector('#reset');
+const inputs = document.querySelectorAll("input");
+const result = document.querySelector("#result");
+const calculateBtn = document.querySelector("#calculate");
+const resetBtn = document.querySelector("#reset");
 
-inputs.forEach(input => {
-    calculateBtn.addEventListener('click', () => {
-        console.log('clicked');
+inputs.forEach((input) => {
+    calculateBtn.addEventListener("click", () => {
         const value1 = parseFloat(inputs[0].value);
         const value2 = parseFloat(inputs[1].value);
-        const operator =document.querySelector('select').value;
-        console.log(value1, value2, operator);
+        const operator = document.querySelector("select").value;
         let resultValue = 0;
-    
-        if (operator === 'add') {
-        resultValue = value1 + value2;
-        } else if (operator === 'subtract') {
-        resultValue = value1 - value2;
-        } else if (operator === 'multiply') {
-        resultValue = value1 * value2;
-        } else if (operator === 'divide') {
-        resultValue = value1 / value2;
-        }else{
+
+        if (operator === "add") {
+            resultValue = value1 + value2;
+        } else if (operator === "subtract") {
+            resultValue = value1 - value2;
+        } else if (operator === "multiply") {
+            resultValue = value1 * value2;
+        } else if (operator === "divide") {
+            resultValue = value1 / value2;
+        } else {
             resultValue = "Invalid Operator";
         }
-        
-        result.textContent = "Result: " + resultValue;
 
+        result.textContent = "Result: " + resultValue;
     });
 });
 
-
-resetBtn.addEventListener('click', () => {
-    inputs.forEach(input => {
-        input.value = '';
+resetBtn.addEventListener("click", () => {
+    inputs.forEach((input) => {
+        input.value = "";
     });
-    result.textContent = 'Result:';
+    result.textContent = "Result:";
 });
